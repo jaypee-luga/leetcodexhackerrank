be0fe781cc2fd31f431ffbfaa94b83b7e2c56c3a
Jaypee Luga <76092372+jaypee-luga@users.noreply.github.com>
2025-03-10 14:16:23 +0800

    Create staircase.cs

---
+++ staircase.cs
@@ -0,0 +1,51 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for(var y = 0; y < n; y++)
+        {
+            for(var x = 0; x < n; x++)
+            {
+                if(x < (n - (y + 1)))
+                    Console.Write(" ");
+                else
+                    Console.Write("#");
+                
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
