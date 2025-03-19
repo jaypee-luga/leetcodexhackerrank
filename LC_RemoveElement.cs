9f09721df12e48c7192fbae2c7ed42111da7ab70
Jaypee Luga <76092372+jaypee-luga@users.noreply.github.com>
2025-03-20 03:04:41 +0800

    Create LC_RemoveElement.cs

---
+++ LC_RemoveElement.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        
+        int previousIndex = 0;
+        int uniqueIndex = 0;
+        int uniqueNumCtr = 1;
+        for (int currentIndex = 0; currentIndex < nums.Length; currentIndex++)
+        {
+            if(nums.Length == 1)
+                return uniqueNumCtr;
+            
+            if(nums[previousIndex] != nums[currentIndex])
+            {
+                previousIndex = currentIndex;
+                uniqueIndex++;
+                nums[uniqueIndex] = nums[currentIndex];
+                uniqueNumCtr++;
+            }            
+        }
+
+        return uniqueNumCtr;
+    }
+}
+    }
+}
