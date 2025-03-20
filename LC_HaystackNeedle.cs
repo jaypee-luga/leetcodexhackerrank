f9437e347efaa81ae939fab74b08238188f8c6d5
Jaypee Luga <76092372+jaypee-luga@users.noreply.github.com>
2025-03-21 00:02:33 +0800

    Update LC_HaystackNeedle.cs

--- LC_HaystackNeedle.cs
+++ LC_HaystackNeedle.cs
@@ -1,5 +1,12 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        return haystack.IndexOf(needle);
+        //return haystack.IndexOf(needle);
+        
+        for(int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            if(haystack.Substring(i,needle.Length).Equals(needle))
+                return i;
+        }
+        return -1;
     }
 }
